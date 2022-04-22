a10f6d810c63575fabf561cec71c05d35c19340b
Vivianliu015 <98603064+Vivianliu015@users.noreply.github.com>
2022-04-22 10:05:13 -0400

    Update main.js

--- assignments/assignment-4/json/main.js
+++ assignments/assignment-4/json/main.js
@@ -1,47 +1,30 @@
-const db = {
-    id: 'appNi3MB1m3UHlrcy',
-    table: 'Couture',
-    apiKey: 'keyPEtf8vgBOzUiUH'
+const albumContainer = document.getElementById('album-Container');
+
+
+const fetchContent = async (fetchUrl) => {
+    const response = await fetch(fetchUrl).then(data => data.json());
+console.log(response.items);
+
+response.items.forEach(item => {
+   console.log(item);
+   const imgEl= document.createElement('img')
+   imgEl.setAttribute('src', item.images[0]     .url);
+   albumContainer.append(imgEl);
+
+});
+
 };
 
-const fetchPrince = async () => {
-    const response = await fetch('https://interactionlab.space/data/assignment-4-1.json').then(data => data.json());
-console.log(response);
 
-    const myObject = {
-        title: 'Prince',
-        release_date: '334824',
-    }
+const buildAlbums = albums=> {
+    response.items.forEach(item => {
+        console.log(item);
+        const imgEl= document.createElement('img');
+        imgEl.setAttribute('src', item.images[0]     .url);
+        albumContainer.append(imgEl);
 
-    const myArray = ['movie', 348927, 'fdiosj'];
+})
 
-    const isReleased = true;
-
-    console.log( myObject.release_date )
-    console.log(myArray[2]);
-
-    const container = document.getElementById('movies-container');
-
-    response.records.forEach((Prince) => {
-        console.log(Prince);
-        if (Prince.fields.poster) {
-            console.log(movie.fields.poster[0].url);
-            const posterImg = document.createElement('img');
-            posterImg.src = movie.fields.poster[0].url;
-            //posterImg.setAttribute('src', movie.fields.poster[0].url);
-            container.append(posterImg);
-        }
-        if (Prince.fields.album_group) {
-            console.log(Prince.fields.album_group);
-        }
-
-        if (movie.fields.description) {
-            const descriptionEl = document.createElement('p');
-            descriptionEl.innerHTML = movie.fields.description;
-            descriptionEl.classList.add('movie-description');
-            container.append(descriptionEl);
-        }
-    });
 };
 
-fetchPrince();
+fetchContent('https://interactionlab.space/data/assignment-4-1.json');
